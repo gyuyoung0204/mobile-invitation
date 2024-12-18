2a48d885ccb8f00b128998b5e674584e0a6b5d4d
dgkim <dgkim@dsr.com>
2024-12-18 13:35:05 +0900

    수정본반영

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  base: '/mobile-invitation/',
+  base: 'https://github.com/gyuyoung0204/mobile-invitation',
   plugins: [react(), tsconfigPaths(), svgr()],
 });
