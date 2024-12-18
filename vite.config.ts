3480cbcd7860aee2044488bf663221b40241cf10
dgkim <dgkim@dsr.com>
2024-12-18 12:39:58 +0900

    수정10

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  base: '/mobile-invitation/',
   plugins: [react(), tsconfigPaths(), svgr()],
 });
