7b5e7b9bd12a327188c6c51ebaad9789b6d3b053
dgkim <dgkim@dsr.com>
2024-12-18 14:37:46 +0900

    ㅅㅈ

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  base: 'https://gyuyoung0204.github.io/mobile-invitation/',
+  base: '/mobile-invitation/',
   plugins: [react(), tsconfigPaths(), svgr()],
 });
