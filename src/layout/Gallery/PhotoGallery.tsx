46d36738d9b442bafa0d98db34c008965cb19fe3
dgkim <dgkim@dsr.com>
2024-12-19 11:24:14 +0900

    추가 기능

--- src/layout/Gallery/PhotoGallery.tsx
+++ src/layout/Gallery/PhotoGallery.tsx
@@ -13,11 +13,13 @@ const PhotoGallery = () => {
   return (
     <Gallery
       options={{
-        zoom: false, // 줌 비활성화
-        maxZoomLevel: 1, // 최대 확대 비율 1로 제한
-        wheelToZoom: false, // 마우스 휠 확대/축소 비활성화
-        pinchToClose: false, // 핀치 제스처로 닫기 비활성화
-        closeOnVerticalDrag: true, // 세로 드래그로 닫기 활성화
+        zoom: false, // 줌 버튼 비활성화
+        maxZoomLevel: 1, // 확대 비율 제한
+        pinchToZoom: false, // 핀치 줌 비활성화
+        wheelToZoom: false, // 마우스 휠 줌 비활성화
+        allowPanToNext: false, // 이미지를 드래그로 이동하지 못하도록 설정
+        doubleTapZoom: false, // 더블 탭 줌 비활성화
+        imageClickAction: 'close', // 이미지를 클릭하면 닫히도록 설정
       }}>
       <div
         style={{
