efbc6aa4a11c0440056d1524aae7415e8165cec5
dgkim <dgkim@dsr.com>
2024-12-27 08:28:45 +0900

    ㅅㅈ

--- src/layout/Location/Location.tsx
+++ src/layout/Location/Location.tsx
@@ -23,12 +23,12 @@ export default Location;
 const LocationWrapper = styled.div`
   width: 100%; /* 모바일 화면에서 가득 채우기 */
   max-width: 1200px; /* 데스크톱 화면에서 최대 너비 제한 */
-  margin: 0 auto; /* 가운데 정렬 */
+  margin: 20 auto; /* 가운데 정렬 */
   display: flex;
   flex-direction: column;
   padding: 20px; /* 내부 여백 추가 */
 
-  @media (max-width: 600px) {
+  @media (max-width: 768px) {
     padding: 10px; /* 모바일 화면에서 패딩 줄이기 */
   }
 `;
