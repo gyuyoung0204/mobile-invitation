8f12b60181eea7b7d642e60295c4ddd096443ae8
dgkim <dgkim@dsr.com>
2024-12-27 12:03:59 +0900

    tw

--- src/layout/Location/Location.tsx
+++ src/layout/Location/Location.tsx
@@ -1,34 +1,26 @@
-import styled from '@emotion/styled';
+import React from 'react';
+import './location.css';
 import data from 'data.json';
 import Address from './Address.tsx';
 import Map from './Map.tsx';
 import MapButtons from './MapButtons.tsx';
-import { Caption } from '@/components/Text.tsx';
 
 const Location = () => {
   const { mapInfo } = data;
   return (
-    <LocationWrapper>
-      <Caption textAlign={'center'}>{mapInfo.address2}</Caption>
-      <Map />
-      <MapButtons />
-      <Address />
-    </LocationWrapper>
+    <div className="location-container">
+      <p className="location-caption">{mapInfo.address2}</p>
+      <div className="map-container">
+        <Map />
+      </div>
+      <div className="map-buttons">
+        <MapButtons />
+      </div>
+      <div className="location-address">
+        <Address />
+      </div>
+    </div>
   );
 };
 
 export default Location;
-
-const LocationWrapper = styled.div`
-  width: 100%; /* 모바일 화면에서 가득 채우기 */
-  max-width: 1200px; /* 데스크톱 화면에서 최대 너비 제한 */
-  margin:210 auto; /* 가운데 정렬 */
-  display: flex;
-  flex-direction: column;
-  padding: 0px; /* 내부 여백 추가 */
-
-  @media (max-width: 600px) {
-    padding: 10px; /* 모바일 화면에서 패딩 줄이기 */
-    max-width: 100%; /* 모바일에서 너비 제한 없이 화면 가득 채우기 */
-  }
-`;
