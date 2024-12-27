f47658f10dfb49139dcd9c7a7e2b6becd28c1b78
dgkim <dgkim@dsr.com>
2024-12-27 13:14:56 +0900

    TW

--- src/layout/Location/Location.tsx
+++ src/layout/Location/Location.tsx
@@ -3,14 +3,13 @@ import data from 'data.json';
 import Address from './Address.tsx';
 import Map from './Map.tsx';
 import MapButtons from './MapButtons.tsx';
-import { Caption, PointTitle } from '@/components/Text.tsx';
+import { Caption } from '@/components/Text.tsx';
 
 const Location = () => {
   const { mapInfo } = data;
 
   return (
     <div className="location-container">
-      <PointTitle>{mapInfo.address1}</PointTitle>
       <Caption textAlign={'center'}>{mapInfo.address2}</Caption>
       <div className="map-container">
         <Map />
