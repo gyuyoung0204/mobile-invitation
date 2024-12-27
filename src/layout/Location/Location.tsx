bae4271ccd70508a638d87c69e6dee1961cc683d
dgkim <dgkim@dsr.com>
2024-12-27 09:01:17 +0900

    ㅅㅈ

--- src/layout/Location/Location.tsx
+++ src/layout/Location/Location.tsx
@@ -1,4 +1,4 @@
-import './Location.css'; // Location.css 파일을 임포트
+import './Location.css';
 import data from 'data.json';
 import Address from './Address.tsx';
 import Map from './Map.tsx';
@@ -8,12 +8,23 @@ const Location = () => {
   const { mapInfo } = data;
   return (
     <div className="location-container">
+      {/* 제목 */}
       <h1 className="location-title">{mapInfo.address1}</h1>
+
+      {/* 캡션 */}
       <p className="location-caption">{mapInfo.address2}</p>
+
+      {/* 지도 */}
       <div className="map-container">
         <Map />
       </div>
-      <MapButtons />
+
+      {/* 버튼 */}
+      <div className="map-buttons">
+        <MapButtons />
+      </div>
+
+      {/* 주소 */}
       <Address />
     </div>
   );
