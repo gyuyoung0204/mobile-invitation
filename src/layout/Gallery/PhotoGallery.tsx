d45186b5e80b29b193d9a8801b1fdac243e1f908
dgkim <dgkim@dsr.com>
2024-12-30 16:24:24 +0900

    tt

--- src/layout/Gallery/PhotoGallery.tsx
+++ src/layout/Gallery/PhotoGallery.tsx
@@ -1,31 +1,25 @@
 import { Gallery, Item } from 'react-photoswipe-gallery';
 import 'photoswipe/style.css';
 import images from '@/layout/Gallery/Images.ts';
+import './Gallery.css';
 
 const PhotoGallery = () => {
   const smallItemStyles: React.CSSProperties = {
     cursor: 'pointer',
-    objectFit: 'cover', // 이미지 비율 유지
+    objectFit: 'cover', // 비율 유지하면서 가득 채우기
     width: '100%',
-    height: 'auto',
+    height: '100%',
   };
 
   return (
     <Gallery
       options={{
-        zoom: false, // 확대 버튼 비활성화
-        maxZoomLevel: 1, // 최대 확대 비율 제한
+        zoom: false, // 확대 비활성화
+        maxZoomLevel: 1, // 확대 비율 제한
         wheelToZoom: false, // 마우스 휠 확대 비활성화
         clickToCloseNonZoomable: true, // 줌 불가능한 이미지 클릭 시 닫기
-      }}
-    >
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(2, 1fr)', // 2개씩 이미지 표시
-          gridGap: '10px',
-        }}
-      >
+      }}>
+      <div className="gallery-grid">
         {images.map((image, index) => (
           <Item
             key={index}
@@ -33,15 +27,14 @@ const PhotoGallery = () => {
             original={image.source}
             thumbnail={image.source}
             width={image.width}
-            height={image.height}
-          >
+            height={image.height}>
             {({ ref, open }) => (
               <img
                 style={smallItemStyles}
                 alt={image.alt}
                 src={image.source}
                 ref={ref as React.MutableRefObject<HTMLImageElement>}
-                onClick={open} // 클릭하면 확대
+                onClick={open} // 클릭하면 오픈
               />
             )}
           </Item>
