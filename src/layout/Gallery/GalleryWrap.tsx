2537bf896aed4322ed99b04ff0ba769a2c3af624
dgkim <dgkim@dsr.com>
2024-12-30 17:23:30 +0900

    tw

--- src/layout/Gallery/GalleryWrap.tsx
+++ src/layout/Gallery/GalleryWrap.tsx
@@ -1,47 +1,28 @@
-import { Gallery, Item } from 'react-photoswipe-gallery';
-import 'photoswipe/style.css';
-import images from '@/layout/Gallery/Images.ts';
+import { useState } from 'react';
+import PhotoGallery from './PhotoGallery.tsx';
 import './Gallery.css';
 
-const PhotoGallery = () => {
-  const smallItemStyles: React.CSSProperties = {
-    cursor: 'pointer',
-    objectFit: 'cover', // 비율 유지하면서 가득 채우기
-    width: '100%',
-    height: '100%',
+const GalleryWrap = () => {
+  const [isMoreView, setIsMoreView] = useState(false);
+
+  const onClickImageMoreViewButton = () => {
+    setIsMoreView(!isMoreView);
   };
 
   return (
-    <Gallery
-      options={{
-        zoom: false, // 확대 비활성화
-        maxZoomLevel: 1, // 확대 비율 제한
-        wheelToZoom: false, // 마우스 휠 확대 비활성화
-        clickToCloseNonZoomable: true, // 줌 불가능한 이미지 클릭 시 닫기
-      }}>
-      <div className="gallery-grid">
-        {images.map((image, index) => (
-          <Item
-            key={index}
-            cropped
-            original={image.source}
-            thumbnail={image.source}
-            width={image.width}
-            height={image.height}>
-            {({ ref, open }) => (
-              <img
-                style={smallItemStyles}
-                alt={image.alt}
-                src={image.source}
-                ref={ref as React.MutableRefObject<HTMLImageElement>}
-                onClick={open} // 클릭하면 오픈
-              />
-            )}
-          </Item>
-        ))}
+    <div className="contents-wrap">
+      <div className={`image-more-wrap ${isMoreView ? 'is-more-view' : ''}`}>
+        {!isMoreView && <div className="white-gradient-overlay" />}
+        <PhotoGallery />
       </div>
-    </Gallery>
+      {!isMoreView && (
+        <div className="plus-button" onClick={onClickImageMoreViewButton}>
+          더보기
+        </div>
+      )}
+    </div>
   );
 };
 
-export default PhotoGallery;
+
+export default GalleryWrap;
