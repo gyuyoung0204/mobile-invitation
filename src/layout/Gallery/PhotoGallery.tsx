7c0e49dc4b5190b829c05cee0739df25d1a6d0df
dgkim <dgkim@dsr.com>
2024-12-30 18:11:52 +0900

    tse

--- src/layout/Gallery/PhotoGallery.tsx
+++ src/layout/Gallery/PhotoGallery.tsx
@@ -3,11 +3,15 @@ import 'photoswipe/style.css';
 import images from '@/layout/Gallery/Images.ts';
 
 const PhotoGallery = () => {
+  // 모바일 여부를 확인하는 함수
+  const isMobile = window.innerWidth <= 768;
+
+  // 스타일 설정
   const smallItemStyles: React.CSSProperties = {
     cursor: 'pointer', // 클릭 가능하도록 설정
     objectFit: 'contain',
-    width: '200px',
-    height: '300px',
+    width: isMobile ? '150px' : '200px', // 모바일에서 크기를 줄임
+    height: isMobile ? '225px' : '300px', // 모바일에서 크기를 줄임
   };
 
   return (
@@ -19,22 +23,20 @@ const PhotoGallery = () => {
         clickToCloseNonZoomable: true, // 줌 불가능한 이미지 클릭 시 닫기
       }}
       onBeforeOpen={(pswpInstance) => {
-        // 줌 동작 비활성화
         pswpInstance.on('uiRegister', () => {
           pswpInstance.options.maxZoomLevel = 1; // 줌 비율 제한
         });
 
-    
-
-        // 줌 메서드 오버라이드
         pswpInstance.zoomTo = () => {}; // 줌 메서드를 비활성화
-      }}>
+      }}
+    >
       <div
         style={{
           display: 'grid',
           gridTemplateColumns: 'repeat(2, 1fr)',
           gridGap: 2,
-        }}>
+        }}
+      >
         {images.map((image, index) => {
           return (
             <Item
@@ -43,7 +45,8 @@ const PhotoGallery = () => {
               original={image.source}
               thumbnail={image.source}
               width={image.width}
-              height={image.height}>
+              height={image.height}
+            >
               {({ ref, open }) => (
                 <img
                   style={smallItemStyles}
