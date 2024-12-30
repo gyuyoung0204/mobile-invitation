a74f7f44495cf31fb152b68d7998716070ef869b
dgkim <dgkim@dsr.com>
2024-12-30 16:16:39 +0900

    tw

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -15,65 +15,65 @@ const images = [   /* 이미지 경로를 리스트로 저장 */
   {
     alt: 'image01',
     source: image01,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },
   {
     alt: 'image02',
     source: image02,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },
   {
     alt: 'image03',
     source: image03,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },{
     alt: 'image04',
     source: image04,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },{
     alt: 'image05',
     source: image05,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },{
     alt: 'image06',
     source: image06,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },{
     alt: 'image07',
     source: image07,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },{
     alt: 'image08',
     source: image08,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },{
     alt: 'image09',
     source: image09,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },{
     alt: 'image10',
     source: image10,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },{
     alt: 'image11',
     source: image11,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   },{
     alt: 'image12',
     source: image12,
-    width: 640,
-    height: 960
+    width: 3,
+    height: 4,
   }
 ];
 
