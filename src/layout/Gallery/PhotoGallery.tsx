fc19a1a7f7da5c7fed49b40f5446e735bc7842de
dgkim <dgkim@dsr.com>
2024-12-30 17:23:36 +0900

    11

--- src/layout/Gallery/PhotoGallery.tsx
+++ src/layout/Gallery/PhotoGallery.tsx
@@ -1,44 +1,61 @@
 import { Gallery, Item } from 'react-photoswipe-gallery';
 import 'photoswipe/style.css';
 import images from '@/layout/Gallery/Images.ts';
-import './Gallery.css';
 
 const PhotoGallery = () => {
   const smallItemStyles: React.CSSProperties = {
-    cursor: 'pointer',
-    objectFit: 'cover', // 비율 유지하면서 가득 채우기
-    width: '100%',
-    height: '100%',
+    cursor: 'pointer', // 클릭 가능하도록 설정
+    objectFit: 'contain',
+    width: '250px',
+    height: '400px',
   };
 
   return (
     <Gallery
       options={{
-        zoom: false, // 확대 비활성화
-        maxZoomLevel: 1, // 확대 비율 제한
+        zoom: false, // 확대 버튼 비활성화
+        maxZoomLevel: 1, // 최대 확대 비율 제한
         wheelToZoom: false, // 마우스 휠 확대 비활성화
         clickToCloseNonZoomable: true, // 줌 불가능한 이미지 클릭 시 닫기
+      }}
+      onBeforeOpen={(pswpInstance) => {
+        // 줌 동작 비활성화
+        pswpInstance.on('uiRegister', () => {
+          pswpInstance.options.maxZoomLevel = 1; // 줌 비율 제한
+        });
+
+    
+
+        // 줌 메서드 오버라이드
+        pswpInstance.zoomTo = () => {}; // 줌 메서드를 비활성화
       }}>
-      <div className="gallery-grid">
-        {images.map((image, index) => (
-          <Item
-            key={index}
-            cropped
-            original={image.source}
-            thumbnail={image.source}
-            width={image.width}
-            height={image.height}>
-            {({ ref, open }) => (
-              <img
-                style={smallItemStyles}
-                alt={image.alt}
-                src={image.source}
-                ref={ref as React.MutableRefObject<HTMLImageElement>}
-                onClick={open} // 클릭하면 오픈
-              />
-            )}
-          </Item>
-        ))}
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, 1fr)',
+          gridGap: 2,
+        }}>
+        {images.map((image, index) => {
+          return (
+            <Item
+              key={index}
+              cropped
+              original={image.source}
+              thumbnail={image.source}
+              width={image.width}
+              height={image.height}>
+              {({ ref, open }) => (
+                <img
+                  style={smallItemStyles}
+                  alt={image.alt}
+                  src={image.source}
+                  ref={ref as React.MutableRefObject<HTMLImageElement>}
+                  onClick={open} // 클릭하면 오픈되도록 유지
+                />
+              )}
+            </Item>
+          );
+        })}
       </div>
     </Gallery>
   );
