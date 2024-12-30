5f54ec702e52fb6a3d34766a695a0b44a8c0830d
dgkim <dgkim@dsr.com>
2024-12-30 17:05:52 +0900

    tw

--- src/layout/Gallery/GalleryWrap.tsx
+++ src/layout/Gallery/GalleryWrap.tsx
@@ -1,26 +1,47 @@
-import { useState } from 'react';
-import PhotoGallery from './PhotoGallery.tsx';
+import { Gallery, Item } from 'react-photoswipe-gallery';
+import 'photoswipe/style.css';
+import images from '@/layout/Gallery/Images.ts';
 import './Gallery.css';
 
-const GalleryWrap = () => {
-  const [isMoreView, setIsMoreView] = useState(false);
-
-  const onClickImageMoreViewButton = () => {
-    setIsMoreView(!isMoreView);
+const PhotoGallery = () => {
+  const smallItemStyles: React.CSSProperties = {
+    cursor: 'pointer',
+    objectFit: 'cover', // 비율 유지하면서 가득 채우기
+    width: '100%',
+    height: '100%',
   };
 
   return (
-    <div className="gallery-container">
-      <div className={`image-more-wrap ${isMoreView ? 'is-more-view' : ''}`}>
-        <PhotoGallery />
+    <Gallery
+      options={{
+        zoom: false, // 확대 비활성화
+        maxZoomLevel: 1, // 확대 비율 제한
+        wheelToZoom: false, // 마우스 휠 확대 비활성화
+        clickToCloseNonZoomable: true, // 줌 불가능한 이미지 클릭 시 닫기
+      }}>
+      <div className="gallery-grid">
+        {images.map((image, index) => (
+          <Item
+            key={index}
+            cropped
+            original={image.source}
+            thumbnail={image.source}
+            width={image.width}
+            height={image.height}>
+            {({ ref, open }) => (
+              <img
+                style={smallItemStyles}
+                alt={image.alt}
+                src={image.source}
+                ref={ref as React.MutableRefObject<HTMLImageElement>}
+                onClick={open} // 클릭하면 오픈
+              />
+            )}
+          </Item>
+        ))}
       </div>
-      {!isMoreView && (
-        <button className="plus-button" onClick={onClickImageMoreViewButton}>
-          더보기
-        </button>
-      )}
-    </div>
+    </Gallery>
   );
 };
 
-export default GalleryWrap;
+export default PhotoGallery;
