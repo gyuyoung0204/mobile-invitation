4917e92ae457e8bf607a091601de0f408c473e22
kim donggyu <dgkim@dsr.com>
2025-01-21 08:48:14 +0900

    tw

--- src/layout/Gallery/PhotoGallery.tsx
+++ src/layout/Gallery/PhotoGallery.tsx
@@ -5,22 +5,29 @@ import images from '@/layout/Gallery/Images.ts';
 const PhotoGallery = () => {
   // 모바일 여부를 확인하는 함수
   const isMobile = window.innerWidth <= 768;
+
+  // 스타일 설정
   const smallItemStyles: React.CSSProperties = {
     cursor: 'pointer', // 클릭 가능하도록 설정
-    objectFit: 'cover', // 이미지 비율을 정사각형에 맞게 채움
+    objectFit: 'cover',
     width: isMobile ? '110px' : '150px', // 모바일: 120px, 데스크톱: 170px
     height: isMobile ? '110px' : '150px', // 정사각형 크기 유지
-    margin: 0, // 여백 약간 추가
   };
-  
-  
 
   return (
     <Gallery
       options={{
-        zoom: false, // 확대 버튼 활성화
-        wheelToZoom: false, // 마우스 휠로 확대 가능
-        clickToCloseNonZoomable: true, // 줌 불가능한 이미지 클릭 시 닫기    
+        zoom: false, // 확대 버튼 비활성화
+        maxZoomLevel: 1, // 최대 확대 비율 제한
+        wheelToZoom: false, // 마우스 휠 확대 비활성화
+        clickToCloseNonZoomable: true, // 줌 불가능한 이미지 클릭 시 닫기
+      }}
+      onBeforeOpen={(pswpInstance) => {
+        pswpInstance.on('uiRegister', () => {
+          pswpInstance.options.maxZoomLevel = 1; // 줌 비율 제한
+        });
+
+        pswpInstance.zoomTo = () => {}; // 줌 메서드를 비활성화
       }}
     >
       <div
@@ -34,21 +41,19 @@ const PhotoGallery = () => {
           return (
             <Item
               key={index}
-              cropped={true} // 원본 비율 유지
-              original={image.source} // 클릭 시 표시할 원본 이미지
-              thumbnail={image.source} // 작은 이미지
+              cropped={false} // 원본 비율 유지
+              original={image.source}
+              thumbnail={image.source}
               width={image.width}
               height={image.height}
-             
             >
               {({ ref, open }) => (
                 <img
-                  style={smallItemStyles} // 작은 이미지 스타일
+                  style={smallItemStyles}
                   alt={image.alt}
                   src={image.source}
                   ref={ref as React.MutableRefObject<HTMLImageElement>}
-                  
-                  onClick={open} // 클릭하면 원본 이미지로 오픈
+                  onClick={open} // 클릭하면 오픈되도록 유지
                 />
               )}
             </Item>
