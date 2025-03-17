acf5a2ec2c888e1571b1687c1beb07a09dec5452
kim donggyu <dgkim@dsr.com>
2025-03-17 10:02:45 +0900

    test

--- src/layout/Gallery/GalleryWrap.tsx
+++ src/layout/Gallery/GalleryWrap.tsx
@@ -1,16 +1,7 @@
 import { useState } from 'react';
-import PhotoGallery from './PhotoGallery.tsx';
+import PhotoGallery from './PhotoGallery.tsx'; // ✅ 기존 PhotoGallery 유지
 import './Gallery.css';
-import 'aos/dist/aos.css';
-import AOS from 'aos';
 
-// AOS 초기화
-AOS.init({
-  duration: 1500, // 애니메이션 지속 시간
-  offset: 100,    // 애니메이션 시작 위치
-  easing: 'ease-in-out', // 부드러운 애니메이션
-  once: true,     // 한 번만 실행
-});
 const GalleryWrap = () => {
   const [isMoreView, setIsMoreView] = useState(false);
 
@@ -19,10 +10,10 @@ const GalleryWrap = () => {
   };
 
   return (
-    <div className="contents-wrap" data-aos="fade-up" data-aos-delay="0">
+    <div className="contents-wrap">
       <div className={`image-more-wrap ${isMoreView ? 'is-more-view' : ''}`}>
         {!isMoreView && <div className="white-gradient-overlay" />}
-        <PhotoGallery />
+        <PhotoGallery isMoreView={isMoreView} />
       </div>
       {!isMoreView && (
         <div className="plus-button" onClick={onClickImageMoreViewButton}>
@@ -32,6 +23,5 @@ const GalleryWrap = () => {
     </div>
   );
 };
-
 
 export default GalleryWrap;
